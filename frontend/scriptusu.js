472301cce1be2ee00aecab35a6eda4b5837cdcd3
nic0l4sB <nitrolaasz@gmail.com>
2025-05-15 11:41:56 -0500

    subida archivos 2

--- frontend/scriptusu.js
+++ frontend/scriptusu.js
@@ -21,7 +21,7 @@ function guardar(){
       redirect: "follow"
     };
  
-    fetch("https://ejemplodesarrollosoft.netlify.app/", requestOptions)
+    fetch("https://softwaresegurosd.netlify.app/", requestOptions)
       .then((response) => response.text())
       .then((result) => console.log(result))
       .catch((error) => console.error(error));
@@ -48,7 +48,7 @@ function listar(){
     let ndoc = document.getElementById("numdoc").value;
     //usuarios?id=user124
          //https://proyectofinaldsws.netlify.app/.netlify/functions/usuarios
-    fetch("https://ejemplodesarrollosoft.netlify.app/"+ndoc, requestOptions)
+    fetch("https://softwaresegurosd.netlify.app/"+ndoc, requestOptions)
       .then((response) =>
         response.text())
       .then((result) =>
